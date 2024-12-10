a6dcd4c99e40246066105d7179f21fbd52f99d15
Daviti Petriashvili <dape00007@stud.uni-saarland.de>
2024-12-10 11:37:17 +0100

    Add D903i

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -1092,6 +1092,7 @@ DEVICES = [
     Device("d902i", 0x06d3, 0x20b0, {"dump_java": FujitsuJavaDumper()}),
     Device("d902is", 0x06d3, 0x2120, {"dump_java": FujitsuJavaDumper()}),
     Device("d702i", 0x06d3, 0x2100, {"dump_java": FujitsuJavaDumper()}),
+    Device("d903i", 0x06d3, 0x2140, {"dump_java": FujitsuJavaDumper()}),
     Device("d703i", 0x06d3, 0x2160, {"dump_java": FujitsuJavaDumper()}),
     Device("d903itv", 0x06d3, 0x2170, {"dump_java": FujitsuJavaDumper()}),
     Device("d904i", 0x06d3, 0x2190, {"dump_java": FujitsuJavaDumper()}),
